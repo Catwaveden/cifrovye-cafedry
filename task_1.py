b368ca8809351f112daf2e0f6d7730ae753f60a6
Catwave <153754311+Catwaveden@users.noreply.github.com>
2024-11-20 18:34:12 +0200

    Update task_1.py
    Исправила недочет в задании 1, поменяла цикл на генератор

--- task_1.py
+++ task_1.py
@@ -8,10 +8,7 @@ FILENAME = "input.json"
 def task() -> float:
     with open(FILENAME) as input_file:
         json_data = json.load(input_file)
-    total_multiplication_sum = 0
-    for item in json_data:
-        total_multiplication_sum += item["score"] * item["weight"]
+    total_multiplication_sum = sum([item["score"] * item["weight"] for item in json_data])
     return round(total_multiplication_sum, 3)
 
 print(task())
-
